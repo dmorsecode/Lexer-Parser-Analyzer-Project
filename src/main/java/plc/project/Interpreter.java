3c6a1bd1dddfc0ac4ea13808134b93b9817a0f6e
dmorsecode <morsecoding@protonmail.com>
2023-10-24 01:07:16 -0400

    First bit of progress on Interpreter. Realized better to start from the bottom up, doing easy ones first.

---
+++ src/main/java/plc/project/Interpreter.java
@@ -0,0 +1,182 @@
+package plc.project;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Interpreter implements Ast.Visitor<Environment.PlcObject> {
+
+    private Scope scope = new Scope(null);
+
+    public Interpreter(Scope parent) {
+        scope = new Scope(parent);
+        scope.defineFunction("print", 1, args -> {
+            System.out.println(args.get(0).getValue());
+            return Environment.NIL;
+        });
+    }
+
+    public Scope getScope() {
+        return scope;
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Source ast) {
+        for (Ast.Field field : ast.getFields()) visit(field);
+        for (Ast.Method method : ast.getMethods()) visit(method);
+        List<Environment.PlcObject> args = new ArrayList<>();
+        return scope.lookupFunction("main", 0).invoke(args);
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Field ast) {
+        Environment.PlcObject value;
+        value = ast.getValue().isPresent() ? visit(ast.getValue().get()) : Environment.NIL;
+        scope.defineVariable(ast.getName(), value);
+        return Environment.NIL;
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Method ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Stmt.Expression ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Stmt.Declaration ast) {
+        if (ast.getValue().isPresent()) {
+            scope.defineVariable(ast.getName(), visit(ast.getValue().get()));
+        } else {
+            scope.defineVariable(ast.getName(), Environment.NIL);
+        }
+
+        return Environment.NIL;
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Stmt.Assignment ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Stmt.If ast) {
+        if (requireType(Boolean.class, visit(ast.getCondition()))) {
+            try {
+                scope = new Scope(scope);
+                for (Ast.Stmt stmt : ast.getThenStatements()) {
+                    visit(stmt);
+                }
+            } finally {
+                scope = scope.getParent();
+            }
+        } else {
+            try {
+                scope = new Scope(scope);
+                for (Ast.Stmt stmt : ast.getElseStatements()) {
+                    visit(stmt);
+                }
+            } finally {
+                scope = scope.getParent();
+            }
+        }
+        return Environment.NIL;
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Stmt.For ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Stmt.While ast) {
+        while (requireType(Boolean.class, visit(ast.getCondition()))) {
+            try {
+                scope = new Scope(scope);
+                for (Ast.Stmt stmt : ast.getStatements()) {
+                    visit(stmt);
+                }
+            } finally {
+                scope = scope.getParent();
+            }
+        }
+        return Environment.NIL;
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Stmt.Return ast) {
+        throw new Return(visit(ast.getValue()));
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Expr.Literal ast) {
+        if (ast.getLiteral() == null) {
+            return Environment.NIL;
+        }
+        return Environment.create(ast.getLiteral());
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Expr.Group ast) {
+        return visit(ast.getExpression());
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Expr.Binary ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Expr.Access ast) {
+        if (ast.getReceiver().isPresent()) {
+            return visit(ast.getReceiver().get()).getField(ast.getName()).getValue();
+        }
+        return scope.lookupVariable(ast.getName()).getValue();
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Expr.Function ast) {
+        List<Environment.PlcObject> args = new ArrayList<>();
+        for (Ast.Expr arg : ast.getArguments()) {
+            args.add(visit(arg));
+        }
+        if (ast.getReceiver().isPresent()) {
+            Environment.PlcObject receiver = visit(ast.getReceiver().get());
+            return receiver.callMethod(ast.getName(), args);
+        } else {
+            return scope.lookupFunction(ast.getName(), args.size()).invoke(args);
+        }
+    }
+
+    /**
+     * Helper function to ensure an object is of the appropriate type.
+     */
+    private static <T> T requireType(Class<T> type, Environment.PlcObject object) {
+        if (type.isInstance(object.getValue())) {
+            return type.cast(object.getValue());
+        } else {
+            throw new RuntimeException("Expected type " + type.getName() + ", received " + object.getValue().getClass().getName() + ".");
+        }
+    }
+
+    /**
+     * Exception class for returning values.
+     */
+    private static class Return extends RuntimeException {
+
+        private final Environment.PlcObject value;
+
+        private Return(Environment.PlcObject value) {
+            this.value = value;
+        }
+
+    }
+
+}
