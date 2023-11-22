6d24ceb2e59bf75e4f8e4d84e96af199af016899
dmorsecode <morsecoding@protonmail.com>
2023-11-22 02:18:40 -0500

    Generator supplement.

---
+++ src/main/java/plc/project/Generator.java
@@ -0,0 +1,139 @@
+package plc.project;
+
+import java.io.PrintWriter;
+
+public final class Generator implements Ast.Visitor<Void> {
+
+    private final PrintWriter writer;
+    private int indent = 0;
+
+    public Generator(PrintWriter writer) {
+        this.writer = writer;
+    }
+
+    private void print(Object... objects) {
+        for (Object object : objects) {
+            if (object instanceof Ast) {
+                visit((Ast) object);
+            } else {
+                writer.write(object.toString());
+            }
+        }
+    }
+
+    private void newline(int indent) {
+        writer.println();
+        for (int i = 0; i < indent; i++) {
+            writer.write("    ");
+        }
+    }
+
+    @Override
+    public Void visit(Ast.Source ast) {
+        throw new UnsupportedOperationException(); //TODO
+        return null;
+    }
+
+    @Override
+    public Void visit(Ast.Field ast) {
+        throw new UnsupportedOperationException(); //TODO
+        return null;
+    }
+
+    @Override
+    public Void visit(Ast.Method ast) {
+        throw new UnsupportedOperationException(); //TODO
+        return null;
+    }
+
+    @Override
+    public Void visit(Ast.Stmt.Expression ast) {
+        print(ast.getExpression());
+        print(";");
+        return null;
+    }
+
+    @Override
+    public Void visit(Ast.Stmt.Declaration ast) {
+        print(ast.getVariable().getType().getJvmName(),
+                " ",
+                ast.getVariable().getJvmName());
+        if (ast.getValue().isPresent()) {
+            print(" = ", ast.getValue().get());
+        }
+        print(";");
+        return null;
+    }
+
+    @Override
+    public Void visit(Ast.Stmt.Assignment ast) {
+        throw new UnsupportedOperationException(); //TODO
+        return null;
+    }
+
+    @Override
+    public Void visit(Ast.Stmt.If ast) {
+        throw new UnsupportedOperationException(); //TODO
+        return null;
+    }
+
+    @Override
+    public Void visit(Ast.Stmt.For ast) {
+        throw new UnsupportedOperationException(); //TODO
+        return null;
+    }
+
+    @Override
+    public Void visit(Ast.Stmt.While ast) {
+        print("while (", ast.getCondition(), ") {");
+        if (!ast.getStatements().isEmpty()) {
+            newline(++indent);
+            for (int i = 0; i < ast.getStatements().size(); i++) {
+                if (i != 0) {
+                    newline(indent);
+                }
+                print(ast.getStatements().get(i));
+            }
+            newline(--indent);
+        }
+        print("}");
+        return null;
+    }
+
+    @Override
+    public Void visit(Ast.Stmt.Return ast) {
+        throw new UnsupportedOperationException(); //TODO
+        return null;
+    }
+
+    @Override
+    public Void visit(Ast.Expr.Literal ast) {
+        throw new UnsupportedOperationException(); //TODO
+        return null;
+    }
+
+    @Override
+    public Void visit(Ast.Expr.Group ast) {
+        throw new UnsupportedOperationException(); //TODO
+        return null;
+    }
+
+    @Override
+    public Void visit(Ast.Expr.Binary ast) {
+        throw new UnsupportedOperationException(); //TODO
+        return null;
+    }
+
+    @Override
+    public Void visit(Ast.Expr.Access ast) {
+        throw new UnsupportedOperationException(); //TODO
+        return null;
+    }
+
+    @Override
+    public Void visit(Ast.Expr.Function ast) {
+        throw new UnsupportedOperationException(); //TODO
+        return null;
+    }
+
+}
